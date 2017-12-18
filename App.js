97832cd85d9accb09c4d9650c1b11cf8186683db
Junhyeok Jang <zzoon91@naver.com>
2017-12-19 02:21:17 +0900

    now we can use icons

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import LoginNav from "./Navs/LoginNav";
 
 export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
   render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <LoginNav />
       </View>
     );
   }
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      // Asset.loadAsync([
+      //   require("./assets/images/logo.png"),
+      //   require("./assets/images/logo-white.png"),
+      //   require("./assets/images/noPhoto.jpg"),
+      //   require("./assets/images/photoPlaceholder.png")
+      // ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+
+  _handleFinishLoading = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  };
 }
 
 styles = StyleSheet.create({
